c16f10c1790bf955129e32501de10e8dae2925d9
Zach <147861361+1054009@users.noreply.github.com>
2024-06-11 20:32:29 -0400

    Add move deltas for air and ground

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -4,14 +4,21 @@ extends living_entity
 @export var RUN_SPEED = 550.0
 @export var JUMP_POWER = -400.0
 
+@export var MOVE_DELTA_GROUND = 20
+@export var MOVE_DELTA_AIR = 6
+
 func run_physics(_delta):
-	if Input.is_action_just_pressed("MOVE_JUMP") and is_on_floor():
+	var on_floor = is_on_floor()
+	
+	if Input.is_action_just_pressed("MOVE_JUMP") and on_floor:
 		velocity.y = JUMP_POWER
 
 	var direction = Input.get_axis("MOVE_LEFT", "MOVE_RIGHT")
+	
 	var move_speed = RUN_SPEED if Input.is_action_pressed("MOVE_FAST") else WALK_SPEED
+	var move_delta = MOVE_DELTA_GROUND if on_floor else MOVE_DELTA_AIR
 	
 	if direction:
-		velocity.x = direction * move_speed
+		velocity.x = move_toward(velocity.x, direction * move_speed, move_delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.x = move_toward(velocity.x, 0, move_delta)
