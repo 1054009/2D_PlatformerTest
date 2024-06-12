43c5b46796895ba68e2c0c7ad1b9573a71043bcb
Zach <147861361+1054009@users.noreply.github.com>
2024-06-11 20:39:45 -0400

    Add damage taking and killing to living_entity

--- src/scripts/living_entity.gd
+++ src/scripts/living_entity.gd
@@ -1,10 +1,28 @@
 class_name living_entity
 extends CharacterBody2D
 
+signal damage_taken
+signal killed
+
 @export var MAX_HEALTH = 100
 var health = 0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func take_damage(amount):
+	var old_health = health
+	
+	health -= amount
+	damage_taken.emit(old_health, health)
+	
+	if health <= 0:
+		killed.emit()
+
+func on_killed():
+	queue_free()
+	
+func _ready():
+	self.killed.connect(on_killed)
 
 func run_physics(_delta):
 	pass
